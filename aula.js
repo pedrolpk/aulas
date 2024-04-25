17d4ffc60fe670d3d82b4b6a97fa362035eb6385
pedrolpk <161369014+pedrolpk@users.noreply.github.com>
2024-04-25 00:56:16 -0300

    Update aula.js

--- aula.js
+++ aula.js
@@ -1,10 +1,16 @@
+/* Faça um programa para calcular o valor de uma viagem.;
+Você terá 3 variaveis. Sendo elas;
+1 - Preço do Combustivel;
+2 - Gasto médio de combustivel do carro;
+3 - Distancia em KM da viagem:
 
+Imprima no Console o valor que será gasto de combustivel para realizar esta viagem*/
 
+/* Corolla tá andando bem kkkk*/ 
 
-
-const precoCombustivel = 5.79;
-const kmLitros = 9.5;
-const distanciaEmKm = 125;
+const precoCombustivel = 5.79; /* facada do posto*/
+const kmLitros = 9.5; /* consumo da ferramenta Toyota*/
+const distanciaEmKm = 125; /* distancia para Teresina*/
 
 
 const litrosConsumidos = distanciaEmKm / kmLitros;
